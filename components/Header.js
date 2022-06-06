b90f5c2c1c93428dc79ecae0aab696cb909ef41b
heyyakash <akashsharma2002@gmail.com>
2022-06-06 22:27:56 +0530

    initial commit

---
+++ components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Header = () => {
+  return (
+    <header>
+        <div>
+            <Link href = "/">
+                <img src="" alt="" />
+            </Link>
+        </div>
+        <div></div>
+    </header>
+  )
+}
+
+export default Header
